324c8b057d7cc7ecbea2bd92395cd9cc45a582c6
Alejandro Diaz Betancourt <alejodb@hotmail.com>
2015-07-30 08:17:17 -0500

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,9 @@ router.get('/author', function(req, res) {
   res.render('author', { nombre: 'Alejandro', foto: '/images/bart2.gif' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
